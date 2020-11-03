60fe3f40419e9027105ac0754611eec6da213888
Vitor Guilherme Moreira Pedro <vitor.guilherme@concert.com.br>
2020-11-03 09:58:51 -0300

    crud completo

---
+++ flutter_crud/lib/data/dummy_users.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'Vítor Guilherme',
+      email: 'vitor@email.com',
+      avatarUrl:
+          'https://avatars3.githubusercontent.com/u/45835780?s=460&u=34c02db4efa6fddf81d27bf3c0459f2d0a759ed8&v=4'),
+  '2': const User(
+      id: '2',
+      name: 'Pedro',
+      email: 'pedro@email.com',
+      avatarUrl:
+          'https://avatars3.githubusercontent.com/u/56973332?s=460&u=85d6024a42b0a103d2d34bc1126dddc3c1c5b5e4&v=4'),
+};
---
+++ flutter_crud/lib/views/user_form.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_crud/models/user.dart';
+import 'package:flutter_crud/provider/users.dart';
+import 'package:provider/provider.dart';
+
+class UserForm extends StatefulWidget {
+  @override
+  _UserFormState createState() => _UserFormState();
+}
+
+class _UserFormState extends State<UserForm> {
+  final _form = GlobalKey<FormState>();
+
+  final Map<String, Object> _formData = {};
+
+  void _loadFormData(User user) {
+    if (user != null) {
+      _formData['id'] = user.id;
+      _formData['name'] = user.name;
+      _formData['email'] = user.email;
+      _formData['avatarUrl'] = user.avatarUrl;
+    }
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    final User user = ModalRoute.of(context).settings.arguments;
+    _loadFormData(user);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Formulário de usuário'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: () {
+              bool isValid = _form.currentState.validate();
+              if (isValid) {
+                _form.currentState.save();
+                Provider.of<UsersProvider>(context, listen: false).put(User(
+                  id: _formData['id'],
+                  name: _formData['name'],
+                  email: _formData['email'],
+                  avatarUrl: _formData['avatarUrl'],
+                ));
+                Navigator.of(context).pop();
+              }
+            },
+          )
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(15),
+        child: Form(
+          key: _form,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                initialValue: _formData['name'],
+                decoration: InputDecoration(
+                  labelText: 'Nome',
+                ),
+                validator: (value) {
+                  if (value == null || value.trim().isEmpty) {
+                    return 'Nome é obrigatório';
+                  }
+                  if (value.trim().length < 3) {
+                    return 'Nome precisa de ter mais que 2 caracteres';
+                  }
+
+                  return null;
+                },
+                onSaved: (value) => _formData['name'] = value,
+              ),
+              TextFormField(
+                initialValue: _formData['email'],
+                decoration: InputDecoration(
+                  labelText: 'E-mail',
+                ),
+                onSaved: (value) => _formData['email'] = value,
+              ),
+              TextFormField(
+                initialValue: _formData['avatarUrl'],
+                decoration: InputDecoration(
+                  labelText: 'Avatar url',
+                ),
+                onSaved: (value) => _formData['avatarUrl'] = value,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
